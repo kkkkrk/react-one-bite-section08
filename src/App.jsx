d23e0ed429686d6ee11eb8752258d70dfc96c3bf
김영준 <dudwns1ms1234@gmail.com>
2026-09-01 12:13:27 +0900

    refactor: App.js todo의 useState를 useReducer로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useRef, useReducer } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -23,27 +23,41 @@ const mockData = [
     date: new Date().getTime(),
   },
 ]
+function reducer(state, action){
+  switch(action.type){
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map(item => item.id === action.data ? {...item, isDone: !item.isDone}: item);
+    case "DELETE":
+      return state.filter(item => item.id !== action.data);
+  }
+}
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const id = useRef(3);
   const onCreate = (content) => {
-    const newTodo = {
-      id : 3,
-      isDone : false,
-      content: content,
-      date : new Date().getTime()
-    }
-    setTodos([newTodo, ...todos])
+    dispatch({
+      type: "CREATE",
+      data: {
+        id : id.current++,
+        isDone : false,
+        content: content,
+        date : new Date().getTime()
+      }
+    })
   }
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo) => 
-      todo.id === targetId 
-        ? {...todo, isDone: !todo.isDone} 
-        : todo
-      )     
-    );
+    dispatch({
+      type: "UPDATE",
+      data: targetId    
+  });
   }
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => targetId !== todo.id));
+    dispatch({
+      type: "DELETE",
+      data: targetId
+    })
   }
   return (
     <div className='App'>
---
+++ src/components/Exam.jsx
@@ -0,0 +1,31 @@
+import { useReducer } from "react";
+
+function reducer(state, action){
+    if(action.type === "INCREASE") return state + action.data;
+    if(action.type === "DECREASE") return state + action.data;
+}
+
+const Exam = () => {
+    const [state, dispatch] = useReducer(reducer, 0);
+
+    const onClickPlus = () => {
+        dispatch({
+            type: "INCREASE",
+            data: 1
+        })
+    }
+    const onClickMinus = () => {
+        dispatch({
+            type: "DECREASE",
+            data: -1
+        })
+    }
+    return(
+        <div>
+            <h1>{state}</h1>
+            <button onClick={onClickPlus}>+</button>
+            <button onClick={onClickMinus}>-</button>
+        </div>
+    )
+}
+export default Exam;
